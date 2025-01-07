96e43ecea173e4d85a2a7308fc59050088a819c2
Yanendrajha <yanendrajha37@gmail.com>
2025-01-08 01:27:15 +0530

    Configuration of STOMP & WebSocket

--- src/main/java/realtimeconvo/controllers/ChatController.java
+++ src/main/java/realtimeconvo/controllers/ChatController.java
@@ -1,7 +1,18 @@
 package realtimeconvo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import realtimeconvo.repositories.RoomRepository;
 
 @RestController
 public class ChatController {
+
+    private final RoomRepository roomRepository;
+
+
+    public ChatController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    // For sending & Receiving Messages
 }
